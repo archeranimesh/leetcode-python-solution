9e672cf390f0414379071d5f690ed19409c1e343
Animesh Bhadra <animeshb@archerimagine.com>
2023-07-25 22:07:29 +0530

    👌 IMPROVE: hashmap solution.

--- solution/1.py
+++ solution/1.py
@@ -1,23 +1,13 @@
 # 1. Two Sum
 # https://www.youtube.com/watch?v=KLlXCFG5TnA
+# it will be O(n) for time and O(n) in space, as a hashmap is created.
 nums = [2, 7, 11, 15]
 target = 9
 
-length = len(nums)
-i = 0
-flag = False
+prev_map = {}
 
-while i < length:
-    print(i, "-->", nums[i])
-    j = i
-    while j < length:
-        print("\t", j, "==>", nums[j])
-        target_sum = nums[i] + nums[j]
-        if target_sum == target:
-            flag = True
-            print("\t\t target index", i, j)
-            break
-        j += 1
-    if flag:
-        break
-    i += 1
+for i, n in enumerate(nums):
+    diff = target - n
+    if diff in prev_map:
+        print(prev_map[diff], i)
+    prev_map[n] = i

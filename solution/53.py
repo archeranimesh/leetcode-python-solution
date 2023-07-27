393e575f24a7f718d05542af6c0be8596a54686d
Animesh Bhadra <animeshb@archerimagine.com>
2023-07-27 22:54:09 +0530

    👌 IMPROVE: Solution from neetCode

--- solution/53.py
+++ solution/53.py
@@ -2,20 +2,17 @@
 # https://www.youtube.com/watch?v=5WZl3MMT0Eg
 
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-# nums = [-1]
+nums = [-1]
 
 
 def maxSubArray(nums) -> int:
-    max_sum = float("-inf")
+    max_sum = nums[0]
     current_sum = 0
     for num in nums:
-        current_sum += num
-        print("nums ", num, " sum= ", current_sum)
-        if current_sum > max_sum:
-            max_sum = current_sum
         if current_sum < 0:
             current_sum = 0
-        print("\tnums ", num, " sum= ", current_sum)
+        current_sum += num
+        max_sum = max(current_sum, max_sum)
 
     return max_sum
 

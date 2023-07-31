f5b9bc2922d72cff0faadca9cdde6c63a2e634ac
Animesh Bhadra <animeshb@archerimagine.com>
2023-07-31 15:09:30 +0530

    👌 IMPROVE:  Using Counter from collections

--- solution/242.py
+++ solution/242.py
@@ -5,22 +5,11 @@
 s = "anagram"
 t = "nagaram"
 
+from collections import Counter
+
 
 def isAnagram(s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False
-
-    countS, countT = {}, {}
-
-    for i in range(len(s)):
-        countS[s[i]] = 1 + countS.get(s[i], 0)
-        countT[t[i]] = 1 + countT.get(t[i], 0)
-
-    for c in countS:
-        if countS[c] != countT.get(c, 0):
-            return False
-
-    return True
+    return Counter(s) == Counter(t)
 
 
 print(isAnagram(s, t))
